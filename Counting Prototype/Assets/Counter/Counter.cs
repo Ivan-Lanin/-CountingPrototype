495f6514acbc2654f296d38b730d9a47bb61a879
Ivan-Lanin <ivanandrlanin@gmail.com>
2023-05-28 23:58:48 +0200

    V0.3

--- Counting Prototype/Assets/Counter/Counter.cs
+++ Counting Prototype/Assets/Counter/Counter.cs
@@ -9,6 +9,7 @@ public class Counter : MonoBehaviour
     public Text CounterText;
 
     private int Count = 0;
+    private List<GameObject> eelsInNet = new List<GameObject>();
 
     private void Start()
     {
@@ -17,10 +18,21 @@ public class Counter : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Count += 1;
-        CounterText.text = "Count : " + Count;
-        if (other.gameObject.CompareTag("Eel")) {
-            other.gameObject.SetActive(false);
+        if (other.CompareTag("Eel")) {
+            eelsInNet.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == "Water") {
+            foreach (GameObject eel in eelsInNet) {
+                Count += 1;
+                CounterText.text = "Count : " + Count;
+                Destroy(eel);
+            }
+        }
+        if (other.CompareTag("Eel")) {
+            eelsInNet.Remove(other.gameObject);
         }
     }
 }
